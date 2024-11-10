f8f0bb027507da6eeb3e7b202ea5ba3d9a87ab11
Ashik Mahmud <141851936+zenithashik@users.noreply.github.com>
2024-11-10 07:43:34 +0600

    Create Linear Search.cpp

---
+++ Linear Search.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;  // Return the index if the element is found
+        }
+    }
+    return -1;  // Return -1 if the element is not found
+}
+
+int main() {
+    int arr[] = {5, 3, 8, 4, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target;
+
+    cout << "Enter the number search for: ";
+    cin >> target;
+
+    int result = linearSearch(arr, size, target);
+
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found " << endl;
+    }
+
+    return 0;
+}
